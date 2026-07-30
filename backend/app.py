aa724633baf05ff65afb1f6e92b9f40ad5e321d5
mosalah555 <mme38650@gmail.com>
2026-07-31 00:58:24 +0300

    Re-enable app.run in main block

--- backend/app.py
+++ backend/app.py
@@ -39,14 +39,14 @@ hyper_scaler = joblib.load(BASE_DIR / "hypertension_scaler.joblib")
 #sarting the app settings with flask
 BASE_DIR = pathlib.Path(__file__).resolve().parent        
 FRONTEND_DIR = BASE_DIR.parent / "frontend"               
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
 app = Flask(
     __name__,
     template_folder=str(FRONTEND_DIR),
     static_folder=str(FRONTEND_DIR),
     static_url_path="/static"
 )
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
 #APIs functions 
 @app.route("/") 
 def index():
